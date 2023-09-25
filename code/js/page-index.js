b62f9400570d1b43fd3f6529ce0a9ccfb6211fc7
eelchabab <e.eelchabab@etu.cvm.qc.ca>
2023-09-25 13:12:44 -0400

    update

--- code/js/page-index.js
+++ code/js/page-index.js
@@ -1,10 +1,3 @@
 window.addEventListener('load', () => {
-    let canvas = document.querySelector('.index-canvas');
-    let title = canvas.getContext('2d');
-    title.font = '50px Arial';
-    title.fillStyle = '#fff';
-    title.strokeStyle = '#000';
-    title.lineWidth = 2;
-    
-    title.strokeText('Run Like Hell', 0, 50);
+
 });
