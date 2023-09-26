be1eca39042b0abec45cbc8e2a3a11f2b004e4ac
eelchabab <e.eelchabab@etu.cvm.qc.ca>
2023-09-25 21:19:49 -0400

    Form (connexion, inscription)

--- code/js/page-index.js
+++ code/js/page-index.js
@@ -1,3 +1,23 @@
 window.addEventListener('load', () => {
+    let btn_connexion = document.querySelector('#connexion');
+    let btn_inscription = document.querySelector('#inscription');
 
+    let page_connexion = document.querySelector('.login-box');
+    let confirmation = document.querySelector('.confirmation');
+    let type_connexion = document.querySelector('#type-connexion');
+    let type_connexion_btn = document.querySelector('#type-connexion-btn');
+
+    btn_connexion.addEventListener('click', () => {
+        page_connexion.style.display = 'block';
+        confirmation.style.display = 'none';
+        type_connexion.innerHTML = 'Connexion';
+        type_connexion_btn.innerHTML = 'Connexion';
+    });
+
+    btn_inscription.addEventListener('click', () => {
+        page_connexion.style.display = 'block';
+        confirmation.style.display = 'block';
+        type_connexion.innerHTML = 'Inscription';
+        type_connexion_btn.innerHTML = 'Inscription';
+    });
 });
