31a2c8617487107c31d8b09c4e6ad5cce0257621
Wyllas <93063485+WyllasSidjeno@users.noreply.github.com>
2023-10-02 16:16:53 -0400

    Added error catching to help devs

--- code/DAO/SqliteDao.py
+++ code/DAO/SqliteDao.py
@@ -11,22 +11,28 @@ import os
 import sqlite3
 from enum import auto, Enum
 
+
 class SqliteDao:
     _instance = None
-    conn: sqlite3.Connection = None
-    cursor: sqlite3.Cursor = None
+    __conn: sqlite3.Connection = None
+    __cursor: sqlite3.Cursor = None
 
     class Actions(Enum):
         ADD_PLAYER = auto()
+        """Name, Password"""
+        GET_PLAYER = auto()
+        """Name"""
         ADD_ITEM = auto()
+        GET_ITEM = auto()
 
     class __ScriptType(Enum):
         CREATE = auto()
         DROP = auto()
+        ACTION = auto()
 
     def __new__(cls):
         if cls._instance is None:
-            cls._instance = super(SqliteDao, cls).__new__(cls)
+            cls._instance = super().__new__(cls)
         return cls._instance
 
     def __init__(self):
@@ -44,23 +50,23 @@ class SqliteDao:
 
     def __create_db(self):
         for script in self.__get_scripts(self.__ScriptType.CREATE):
-            self.cursor.executescript(script)
+            self.__cursor.executescript(script)
 
     def __drop_db(self):
         for script in self.__get_scripts(self.__ScriptType.DROP):
-            self.cursor.executescript(script)
+            self.__cursor.executescript(script)
 
     def __connect(self):
-        self.conn = sqlite3.connect(self.path + self.name)
-        self.conn.execute(self.pragma)
-        self.conn.commit()
-        self.cursor = self.conn.cursor()
+        self.__conn = sqlite3.connect(self.path + self.name)
+        self.__conn.execute(self.pragma)
+        self.__conn.commit()
+        self.__cursor = self.__conn.cursor()
 
     def __close(self):
-        if self.cursor:
-            self.cursor.close()
-        if self.conn:
-            self.conn.close()
+        if self.__cursor:
+            self.__cursor.close()
+        if self.__conn:
+            self.__conn.close()
 
     def __get_scripts(self, script_type: Enum):
         scripts = []
@@ -69,9 +75,34 @@ class SqliteDao:
                 scripts.append(f.read())
         return scripts
 
+    def __get_action_script(self, action: Actions):
+        scripts = self.__get_scripts(self.__ScriptType.ACTION)
+        return next(script for script in scripts if action.name in script)
+
     def debug_reset_db(self):
         self.__connect()
         self.__drop_db()
         self.__create_db()
 
+    def request(self, action: Actions, *args):
+        self.__connect()
+        try:
+            script = self.__get_action_script(action)
+            self.__cursor.execute(script, args)
+            self.__conn.commit()
+        except sqlite3.Error as e:
+            print("An SQL error occurred:")
+            print(e)
+        except FileNotFoundError as e:
+            print("A file error occurred:")
+            print(e)
+        except StopIteration as e:
+            print(f'No such file exists : {action}')
+            print(e)
+        finally:
+            self.__close()
 
+
+sqli = SqliteDao()
+sqli.debug_reset_db()
+sqli.request(SqliteDao.Actions.ADD_PLAYER, "test", "test")
