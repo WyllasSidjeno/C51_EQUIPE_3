23ad2869aa81f4a0fd17ee1136f320fe54a1cb87
bibz43 <68484992+Juliettevincent@users.noreply.github.com>
2023-10-22 16:58:38 -0400

    Son menu & son click

--- app/static/page-index.js
+++ app/static/page-index.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', () => {
     let sound = new Sound();
+
     let btn_connexion = document.querySelector('#connexion');
     let btn_inscription = document.querySelector('#inscription');
     let page_inscription = document.querySelector('.inscription-box');
@@ -15,13 +16,15 @@ window.addEventListener('load', () => {
     btn_connexion.addEventListener('click', () => {
         page_connexion.style.display = 'block';
         page_inscription.style.display = 'none';
-        //new Audio(sound.click).play();
+
+        new Audio(sound.click).play();
     });
 
     btn_inscription.addEventListener('click', () => {
         page_inscription.style.display = 'block';
         page_connexion.style.display = 'none';
-        //new Audio(sound.click).play();
+
+        new Audio(sound.click).play();
     });
 
     if (localStorage.getItem('state') === 'success') {
@@ -33,20 +36,16 @@ window.addEventListener('load', () => {
     btn_deconnexion.addEventListener('click', () => {
         localStorage.removeItem('state');
         localStorage.removeItem('username');
+
         btn_connexion.style.display = 'block';
         btn_inscription.style.display = 'block';
         btn_deconnexion.style.display = 'none';
+
+        new Audio(sound.click).play();
     });
 
-    // SON 
-    // let element = document.querySelectorAll(".text");
-    // let clickAudio = new Audio(sound.click);
-    // let menuAudio = new Audio(sound.menu);
-
-    // element.forEach((e) => {
-    //     e.addEventListener('click', () => {
-    //         clickAudio.play();
-    //     });
-    // });
+    let menu_music = new Audio(sound.menu);
+    menu_music.loop = true;
+    menu_music.play();
 
 });
--- app/static/sound.js
+++ app/static/sound.js
@@ -1,8 +1,8 @@
 // Option 1
 class Sound {
     constructor() {
-        this.click = "../static/test.mp3";
-        this.menu = /*"menu-music.mp3";*/
+        this.click = "../static/sound/click.mp3";
+        this.menu = "../static/sound/menu-music.mp3";
         this.donjon = "../static/donjon-music.mp3";
         this.cave = "";
         this.foret = "";
