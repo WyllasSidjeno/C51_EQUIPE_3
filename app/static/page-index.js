2730ea982a5bfeb1444054411844144d1b8afde8
Juliettevincent <68484992+Juliettevincent@users.noreply.github.com>
2023-10-23 11:15:20 -0400

    son on/off bouton

--- app/static/page-index.js
+++ app/static/page-index.js
@@ -6,12 +6,17 @@ window.addEventListener('load', () => {
     let page_inscription = document.querySelector('.inscription-box');
     let page_connexion = document.querySelector('.connexion-box');
     let btn_deconnexion = document.querySelector('#deconnexion');
+    let btn_volume = document.querySelector('#volumeOn');
+    let btn_muted = document.querySelector('#muted');
 
     btn_connexion.style.display = 'block';
     btn_inscription.style.display = 'block';
 
     page_connexion.style.display = 'none';
     page_inscription.style.display = 'none';
+
+    btn_volume.style.display = 'block';
+    btn_muted.style.display = 'none';
 
     btn_connexion.addEventListener('click', () => {
         closeFrame(page_connexion, page_inscription);
@@ -41,11 +46,11 @@ window.addEventListener('load', () => {
         new Audio(sound.click).play();
     });
 
-    const closeFrame = (mainFrame, frame) => {
-        if (frame.style.display === 'block') {
-            toggleFrame(frame);
+    const closeFrame = (showingFrame, hidingFrame) => {
+        if (hidingFrame.style.display === 'block') {
+            toggleFrame(hidingFrame);
         }
-        toggleFrame(mainFrame);
+        toggleFrame(showingFrame);
     }
 
     const toggleFrame = (frame) => {
@@ -56,8 +61,20 @@ window.addEventListener('load', () => {
         }
     }
 
+    btn_muted.addEventListener('click', () => {
+        backgroundMusic(sound, true);
+        closeFrame(btn_volume, btn_muted);
+    })
+    
+    btn_volume.addEventListener('click', () => {
+        backgroundMusic(sound, false);
+        closeFrame(btn_muted, btn_volume);
+    })
+});
+
+const backgroundMusic = (sound, state) =>  {
     let menu_music = new Audio(sound.menu);
     menu_music.loop = true;
-    menu_music.play();
-
-});
+    if(state) menu_music.play();
+    else menu_music.muted = !state;
+}
