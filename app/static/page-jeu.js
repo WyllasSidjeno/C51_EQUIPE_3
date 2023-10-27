ed5a2e87fe925caf571c0a14919d7c98eb18b2bc
eelchabab <e.eelchabab@etu.cvm.qc.ca>
2023-10-26 22:09:53 -0400

    test

--- app/static/page-jeu.js
+++ app/static/page-jeu.js
@@ -10,7 +10,7 @@ window.addEventListener('load', () => {
    fetch("map1.json")
     .then(response => response.json())
     .then(data => {
-
+       console.log(data);
     })
     .catch(error => console.log(error));
 });
