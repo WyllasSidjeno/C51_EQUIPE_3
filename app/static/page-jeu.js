48bd2ec41a50a1194953734fb7a225581dfab9e2
eelchabab <e.eelchabab@etu.cvm.qc.ca>
2023-10-26 22:45:32 -0400

    debut json reading

--- app/static/page-jeu.js
+++ app/static/page-jeu.js
@@ -2,6 +2,7 @@ window.addEventListener('load', () => {
    let topnav = document.querySelector('.topnav');
    let mute = document.querySelector('#muted');
    let volume = document.querySelector('#volumeOn');
+   let canvas = document.querySelector('.canvas');
 
    topnav.style.display = 'none';
    mute.style.display = 'none';
@@ -10,7 +11,7 @@ window.addEventListener('load', () => {
    fetch('/static/map1.json')
     .then(response => response.json())
     .then(data => {
-       console.log(data);
+        console.log(data);
     })
     .catch(error => console.log("JSON parsing error" + error));
 });
