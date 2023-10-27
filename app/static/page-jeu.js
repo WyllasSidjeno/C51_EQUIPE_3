a9bf6fdb11560ee8dc0f1092e06f0110c5f045fe
eelchabab <e.eelchabab@etu.cvm.qc.ca>
2023-10-27 13:17:16 -0400

    test

--- app/static/page-jeu.js
+++ app/static/page-jeu.js
@@ -3,6 +3,8 @@ window.addEventListener('load', () => {
    let mute = document.querySelector('#muted');
    let volume = document.querySelector('#volumeOn');
    let canvas = document.querySelector('.canvas');
+   let floor = document.querySelector('#floor');
+   const ctx = canvas.getContext("2d");
 
    topnav.style.display = 'none';
    mute.style.display = 'none';
@@ -11,7 +13,20 @@ window.addEventListener('load', () => {
    fetch('/static/map1.json')
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        // console.log(data.defs["layers"]);
+
+        const layers = data.defs["layers"];
+        const level = data.levels[0];
+        const floorPos = layers.find(layer => layer.identifier === "Floor");
+        const floorImg = level.layerInstances.find(level => level.__identifier === "Floor");
+        
+        let newFloor = document.createElement('div');
+        floor.appendChild(newFloor);
+        newFloor.classList.add('floor');
+        // newFloor.style.backgroundImage = "url('/static/img/floor.png')";
+        newFloor.style.width = floorPos.gridSize + "px";
+        
+        console.log(floorImg);
     })
     .catch(error => console.log("JSON parsing error" + error));
 });
