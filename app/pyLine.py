5fa5ed26746e57c5bde4a2b950a84abbe4a0b302
Juliettevincent <68484992+Juliettevincent@users.noreply.github.com>
2023-10-30 11:14:49 -0400

    temp show_content

--- app/pyLine.py
+++ app/pyLine.py
@@ -1,10 +1,10 @@
 import os
 import sys
 from DAO.TableCreationDAO import TableCreationDao
-from app.DAO.CommentDAO import CommentDAO
-from app.DAO.UserDAO import UserDAO
+from DAO.CommentDAO import CommentDAO
+from DAO.UserDAO import UserDAO
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 if __name__ == "__main__":
-    CommentDAO().get_comments()
+    TableCreationDao().reset_db()
