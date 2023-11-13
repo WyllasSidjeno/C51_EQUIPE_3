ace2d3d5f95598ccafe8cf995cf5030cd879da43
Wyllas <93063485+WyllasSidjeno@users.noreply.github.com>
2023-11-13 09:42:59 -0500

    DAO Fix

--- app/SQLite/action/get_comments.sql
+++ app/SQLite/action/get_comments.sql
@@ -1,6 +1,4 @@
-SELECT c.id, u.username, c.date_creation, c.message,
-  COALESCE(GROUP_CONCAT(rc.message), 'Nulle') AS rc_messages
+SELECT c.id, u.username, c.date_creation, c.message
 FROM commentaire c
 JOIN User u ON c.user_id = u.id
-LEFT JOIN reponse_commentaire rc ON c.id = rc.commentaire_id
 GROUP BY c.id, u.username, c.date_creation, c.message;
--- app/pyLine.py
+++ app/pyLine.py
@@ -7,4 +7,8 @@ from app.DAO.UserDAO import UserDAO
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 if __name__ == "__main__":
-    CommentDAO().get_comments()
+    comment = CommentDAO()
+    print(comment.get_comments())
+
+    user = UserDAO()
+    print(user.get_user_with_password("admin"))
